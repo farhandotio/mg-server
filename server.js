b1a50a15d7870d63d5683c567a2daaae29dbe039
farhandotio <farhansadik0760@gmail.com>
2026-01-02 02:31:27 +0600

    port add

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 connectDB();
 
 app.listen(port, () => {
