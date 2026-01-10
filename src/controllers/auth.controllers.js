f0e92163cc536694cf0a6c365217115bc7db4c10
farhandotio <farhansadik0760@gmail.com>
2026-01-10 10:25:13 +0600

    logout updated

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -74,8 +74,17 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.cookie('token', '', { expires: new Date(0) });
-  res.status(200).json({ message: 'Logged out successfully' });
+  res.cookie('token', null, {
+    expires: new Date(0), 
+    httpOnly: true, 
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict', 
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'Logged out successfully',
+  });
 };
 
 /** --- PASSWORD MANAGEMENT --- **/
