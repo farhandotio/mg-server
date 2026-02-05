7381aa5aa76690ec80cf59d574e52b04e1625d17
farhandotio <farhansadik0760@gmail.com>
2026-02-05 14:20:25 +0600

    await

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import app from './src/app.js';
 import connectDB from './src/config/db.js';
 import { connectRedis } from './src/config/redis.js';
 
-connectDB().then(() => connectRedis());
+await connectDB()
+await connectRedis();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`🚀 Port: ${port}`));
