45d3783eaa536e260b6ac08d3c542bdadd0a84d1
farhandotio <farhansadik0760@gmail.com>
2026-02-06 00:13:32 +0600

    db fix

--- src/config/db.js
+++ src/config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
 
   try {
     await mongoose.connect(MONGO_URI, {
-      dbName: 'your_db_name', 
+      dbName: 'my-gadget', 
     });
 
     console.log('✅ MongoDB Connected');
